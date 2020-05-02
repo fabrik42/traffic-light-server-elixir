065768c54991d01c89b46e0638411b6e8c0fef23
Christian Bäuerlein <fabrik42@gmail.com>
2020-05-02 21:26:47 +0200

    Add heroku configuration

--- lib/traffic_light_web/endpoint.ex
+++ lib/traffic_light_web/endpoint.ex
@@ -11,10 +11,12 @@ defmodule TrafficLightWeb.Endpoint do
   ]
 
   socket "/socket", TrafficLightWeb.UserSocket,
-    websocket: true,
+    websocket: [timeout: 45_000],
     longpoll: false
 
-  socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
+  socket "/live", Phoenix.LiveView.Socket,
+    websocket: [timeout: 45_000, connect_info: [session: @session_options]],
+    longpoll: false
 
   # Serve at "/" the static files from "priv/static" directory.
   #
