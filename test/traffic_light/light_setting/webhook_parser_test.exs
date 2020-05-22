2fb03fcc2eb38483e16296c0b794970650c637b8
Christian Bäuerlein <fabrik42@gmail.com>
2020-05-22 14:22:29 +0200

    Deduplicate codeship payload test helper

--- test/traffic_light/light_setting/webhook_parser_test.exs
+++ test/traffic_light/light_setting/webhook_parser_test.exs
@@ -2,11 +2,11 @@ defmodule TrafficLight.LightSetting.WebhookParserTest do
   use ExUnit.Case
   doctest TrafficLight.LightSetting.WebhookParser
 
-  alias TrafficLight.LightSetting
+  alias TrafficLight.{Factory, LightSetting}
   alias TrafficLight.LightSetting.WebhookParser
 
   test "from codeship with unknown build sate" do
-    payload = build_payload("another_state")
+    payload = Factory.build(:codeship_payload, %{status: "another_state"})
     assert WebhookParser.from_codeship(payload) == {:error, "Unknown build state: another_state"}
   end
 
@@ -14,7 +14,7 @@ defmodule TrafficLight.LightSetting.WebhookParserTest do
     states = ["error", "stopped", "ignored", "blocked", "infrastructure_failure"]
 
     Enum.each(states, fn state ->
-      payload = build_payload(state)
+      payload = Factory.build(:codeship_payload, %{status: state})
       expected = %LightSetting{green: false, mode: "ci", red: true, yellow: false}
       assert WebhookParser.from_codeship(payload) == {:ok, expected}
     end)
@@ -24,7 +24,7 @@ defmodule TrafficLight.LightSetting.WebhookParserTest do
     states = ["initiated", "waiting", "testing"]
 
     Enum.each(states, fn state ->
-      payload = build_payload(state)
+      payload = Factory.build(:codeship_payload, %{status: state})
       expected = %LightSetting{green: false, mode: "ci", red: false, yellow: true}
       assert WebhookParser.from_codeship(payload) == {:ok, expected}
     end)
@@ -34,32 +34,9 @@ defmodule TrafficLight.LightSetting.WebhookParserTest do
     states = ["success"]
 
     Enum.each(states, fn state ->
-      payload = build_payload(state)
+      payload = Factory.build(:codeship_payload, %{status: state})
       expected = %LightSetting{green: true, mode: "ci", red: false, yellow: false}
       assert WebhookParser.from_codeship(payload) == {:ok, expected}
     end)
-  end
-
-  # Payload taken from https://documentation.codeship.com/general/projects/notifications/
-  def build_payload(status) do
-    Poison.decode!(~s(
-      {
-        "build": {
-          "build_url":"https://www.codeship.com/projects/10213/builds/973711",
-          "commit_url":"https://github.com/codeship/docs/
-          commit/96943dc5269634c211b6fbb18896ecdcbd40a047",
-          "project_id":10213,
-          "build_id":973711,
-          "status":"#{status}",
-          "project_full_name":"codeship/docs",
-          "project_name":"codeship/docs",
-          "commit_id":"96943dc5269634c211b6fbb18896ecdcbd40a047",
-          "short_commit_id":"96943",
-          "message":"Merge pull request #34 from codeship/feature/shallow-clone",
-          "committer":"beanieboi",
-          "branch":"master"
-        }
-      }
-    ))
   end
 end
--- test/traffic_light_web/controllers/webhook/codeship_controller_test.exs
+++ test/traffic_light_web/controllers/webhook/codeship_controller_test.exs
@@ -1,7 +1,7 @@
 defmodule TrafficLightWeb.Webhook.CodeshipControllerTest do
   use TrafficLightWeb.ConnCase
 
-  alias TrafficLight.LightSetting
+  alias TrafficLight.{Factory, LightSetting}
 
   setup %{conn: conn} do
     {:ok,
@@ -13,7 +13,7 @@ defmodule TrafficLightWeb.Webhook.CodeshipControllerTest do
     {:ok, _} = LightSetting.save(red, "ci")
     assert LightSetting.load("ci") == {:ok, red}
 
-    payload = build_payload("success")
+    payload = Factory.build(:codeship_payload, %{status: "success"})
     conn = post(conn, Routes.codeship_path(conn, :create, token: token), payload)
 
     assert %{"success" => true} == json_response(conn, 201)
@@ -27,7 +27,7 @@ defmodule TrafficLightWeb.Webhook.CodeshipControllerTest do
     {:ok, _} = LightSetting.save(red, "ci")
     assert LightSetting.load("ci") == {:ok, red}
 
-    payload = build_payload("UNKNOWN_STATE")
+    payload = Factory.build(:codeship_payload, %{status: "UNKNOWN_STATE"})
     conn = post(conn, Routes.codeship_path(conn, :create, token: token), payload)
 
     expected = %{"success" => false, "error" => "Unknown build state: UNKNOWN_STATE"}
@@ -37,34 +37,9 @@ defmodule TrafficLightWeb.Webhook.CodeshipControllerTest do
   end
 
   test "don't update the build status without valid token", %{conn: conn} do
-    payload = build_payload("UNKNOWN_STATE")
+    payload = Factory.build(:codeship_payload, %{status: "UNKNOWN_STATE"})
     conn = post(conn, Routes.codeship_path(conn, :create), payload)
 
     assert "Unauthorized" == response(conn, 401)
-  end
-
-  # TODO: deduplicate and move to test helpers
-  # idea: build(:codeship_webhook_payload) like factory
-  # Payload taken from https://documentation.codeship.com/general/projects/notifications/
-  def build_payload(status) do
-    Poison.decode!(~s(
-          {
-            "build": {
-              "build_url":"https://www.codeship.com/projects/10213/builds/973711",
-              "commit_url":"https://github.com/codeship/docs/
-              commit/96943dc5269634c211b6fbb18896ecdcbd40a047",
-              "project_id":10213,
-              "build_id":973711,
-              "status":"#{status}",
-              "project_full_name":"codeship/docs",
-              "project_name":"codeship/docs",
-              "commit_id":"96943dc5269634c211b6fbb18896ecdcbd40a047",
-              "short_commit_id":"96943",
-              "message":"Merge pull request #34 from codeship/feature/shallow-clone",
-              "committer":"beanieboi",
-              "branch":"master"
-            }
-          }
-        ))
   end
 end
